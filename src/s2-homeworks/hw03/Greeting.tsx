e2dd7bea06a5caa7979a94f683a6ba1c2a5dc01e
serakss <serak_96@mail.ru>
2022-11-10 17:51:57 +0300

    fdfdfcc

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -25,7 +25,8 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass =error? s.input:s.errorInput // need to fix with (?:)
+    const inputClass =error? s.errorInput:s.input
+
 
     return (
         <div id={'hw3-form'} className={s.greetingForm}>
